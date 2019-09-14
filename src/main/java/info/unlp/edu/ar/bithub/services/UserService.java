c0cce842875780ff2dec54ae453ea2e0ff0a4958
Hekkel <sn2005ap@gmail.com>
2019-09-14 18:20:23 -0300

    dkfnsdkfn

--- src/main/java/info/unlp/edu/ar/bithub/services/UserService.java
+++ src/main/java/info/unlp/edu/ar/bithub/services/UserService.java
@@ -4,10 +4,16 @@ import info.unlp.edu.ar.bithub.model.User;
 import info.unlp.edu.ar.bithub.repositories.UserRepository.UserRepository;
 import org.bson.types.ObjectId;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.inject.Named;
@@ -34,8 +40,34 @@ public class UserService {
         return this.getUserRepository().findAll();
     }
 
+    public List<User> getAllUsersFromElastic(){
+        SearchRequest searchRequest = new SearchRequest("user");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+        try {
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public Optional<User> getUserFromMongo(ObjectId id) {
         return this.getUserRepository().findById(id);
+    }
+
+    public Optional<User> getUserFromElastic(ObjectId id) {
+        SearchRequest searchRequest = new SearchRequest("user");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(new MatchQueryBuilder("id",id));
+        searchRequest.source(searchSourceBuilder);
+        try {
+            SearchResponse searchResponse = client.search(searchRequest,RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public void addUser(String name, String email) {
